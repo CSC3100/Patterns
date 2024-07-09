6923da0d8c3625b054709ce8fe8c4e070997ac2c
javiergs <3814755+javiergs@users.noreply.github.com>
2024-07-09 00:12:19 -0700

    Adding Plotter App as an example of Decorator

---
+++ src/main/java/integrate/plotter/Marker.java
@@ -0,0 +1,16 @@
+package integrate.plotter;
+
+import java.awt.*;
+
+/**
+ * Abstract class that represents a marker object.
+ * This class is part of the decorator pattern.
+ *
+ * @author javiergs
+ * @version 1.0
+ */
+public abstract class Marker {
+	
+    public abstract void draw(Graphics g, int value);
+
+}
